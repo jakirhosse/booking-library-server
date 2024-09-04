d85f64be3abf38d2d46d1aaaedc672d1044738c8
unknown <jakirhossain45951@gmail.com>
2024-09-05 02:49:41 +0600

    code

--- src/Controllers/bookController.js
+++ src/Controllers/bookController.js
@@ -21,14 +21,7 @@ const deleteBook = async (query) => {
   return result;
 };
 
-// get books by user email
-// const getUserBooks = async (email) => {
-//   const client = await connectToMongoDB();
-//   const booksCollection = client.db("LangMaster").collection("bought-books");
-//   const query = { email: email };
-//   const result = await booksCollection.find(query).toArray();
-//   return result;
-// };
+// brouth bookk /////////
 
 //post bought books in db
 const postBoughtBooks = async (bookInfo) => {
@@ -37,9 +30,43 @@ const postBoughtBooks = async (bookInfo) => {
   const result = await booksCollection.insertOne(bookInfo);
   return result;
 };
+// get books by user email
+const getUserBooks = async (email) => {
+  const client = await connectToMongoDB();
+  const booksCollection = client.db("LangMaster").collection("bought-books");
+  const query = { email: email };
+  const result = await booksCollection.find(query).toArray();
+  return result;
+};
+
+// get all bought books
+const getAllBoughtBooks = async () => {
+  const client = await connectToMongoDB();
+  const boughtBooksCollection = client
+    .db("LangMaster")
+    .collection("bought-books");
+  const result = await boughtBooksCollection
+    .find()
+    .sort({ date: -1 })
+    .toArray();
+  return result;
+};
+
+// delete boughtBookDelete
+const boughtBookDelete = async (query) => {
+  const client = await connectToMongoDB();
+  const booksCollection = client.db("LangMaster").collection("bought-books");
+  const result = await booksCollection.deleteOne(query);
+  return result;
+};
+
 module.exports = {
   createBook,
   getAllBooks,
   deleteBook,
   postBoughtBooks,
+  getUserBooks,
+  getAllBooks,
+  getAllBoughtBooks,
+  boughtBookDelete,
 };
--- src/routers/bookRouters.js
+++ src/routers/bookRouters.js
@@ -34,6 +34,27 @@ router.delete("/deleteBook/:id", async (req, res) => {
   }
 });
 
+// get single user books
+router.get("/bought-book", async (req, res) => {
+  const email = req.query.email;
+  try {
+    const result = await bookController.getUserBooks(email);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ error: "error getting all books", error });
+  }
+});
+
+// get all bought books
+router.get("/bought-books", async (req, res) => {
+  try {
+    const result = await bookController.getAllBooks();
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ error: "error getting all books", error });
+  }
+});
+
 // post bought books on db
 router.post("/bought-book", async (req, res) => {
   const bookInfo = req.body.bookInfo;
@@ -44,4 +65,17 @@ router.post("/bought-book", async (req, res) => {
     res.status(500).send({ error: "error posting bought books", error });
   }
 });
+
+// delete bought-books --------------
+router.delete("/bought-books/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const query = { _id: new ObjectId(id) };
+    const bookDelete = await bookController.boughtBookDelete(query);
+    res.status(200).send({ message: "Book deleted", data: bookDelete });
+  } catch (error) {
+    res.status(500).send({ error: "Book Internal Server Error" });
+  }
+});
+
 module.exports = router;
